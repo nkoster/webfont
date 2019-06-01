db511626f7b1591d1c7e6078f3610d2c0c3689bb
Niels Koster <n.koster@portavita.eu>
2019-06-02 01:12:38 +0200

    Upload font, download zip

--- server.js
+++ server.js
@@ -3,30 +3,41 @@
 const
     express = require('express'),
     app = express(),
-    formidable = require('formidable')
+    fs = require('fs'),
+    formidable = require('formidable'),
+    { execFile } = require('child_process')
 
 app.use(express.static('public'))
 
 app.post('/submit-form', (req, res) => {
     new formidable.IncomingForm().parse(req)
-      .on('field', (name, field) => {
-        console.log('Field', name, field)
-      })
-      .on('fileBegin', (_, file) => {
-          file.path = __dirname + '/uploads/' + file.name
-      })
-      .on('file', (name, file) => {
-        console.log('Uploaded file', file.name)
-      })
-      .on('aborted', () => {
-        console.error('Request aborted by the user')
-      })
-      .on('error', (err) => {
-        console.error('Error', err)
-        throw err
-      })
-      .on('end', () => {
-        res.end()
-      })
+        .on('field', (name, field) => {
+            console.log('Field', name, field)
+        })
+        .on('fileBegin', (_, file) => {
+            file.path = __dirname + '/uploads/' + file.name
+        })
+        .on('file', (_, file) => {
+            console.log('Uploaded file', file.path)
+            execFile('/home/niels/bash/webfont.bash', [file.path],
+                (_, stdout) => {
+                    console.log(stdout)
+                    const
+                        zipName = file.name.split('.').slice(0, -1).join('.') + '.zip',
+                        zipPath = file.path.split("/").slice(0,-1).join("/"),
+                        readStream = fs.createReadStream(zipPath + '/' + zipName)
+                    res.setHeader('Content-disposition', 'attachment; filename=' + zipName)
+                    res.setHeader('Content-type', 'application/zip, application/octet-stream, application/x-zip-compressed, multipart/x-zip')
+                    readStream.pipe(res);
+            })
+        })
+        .on('aborted', () => {
+            console.error('Request aborted by the user')
+        })
+        .on('error', (err) => {
+            console.error('Error', err)
+            throw err
+        })
 })
+
 app.listen(9000)
