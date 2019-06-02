32547e36f773d17bd95748b9357235b4bed028b0
Niels Koster <n.koster@portavita.eu>
2019-06-02 15:19:23 +0200

    rm useless header + cleanup

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const
 
 app.use(express.static('public'))
 
-app.post('/submit-form', (req, res) => {
+app.post('/get-zip', (req, res) => {
     new formidable.IncomingForm().parse(req)
         .on('fileBegin', (_, file) => {
             file.path = __dirname + '/uploads/' + file.name
@@ -17,7 +17,6 @@ app.post('/submit-form', (req, res) => {
         .on('file', (_, file) => {
             console.log('Uploaded file', file.path)
             const ext = file.path.split('.').pop().toUpperCase()
-            console.log('ext ', ext)
             if (['TTF', 'OTF', 'EOT', 'SVG', 'WOFF'].includes(ext)) {
                 execFile('/home/niels/bash/webfont.bash', [file.path],
                     (_, stdout) => {
@@ -25,7 +24,6 @@ app.post('/submit-form', (req, res) => {
                         const zipName = file.name.split('.').slice(0, -1).join('.') + '.zip'
                         res.setHeader('Content-disposition', 'attachment; filename=' + zipName)
                         res.setHeader('Content-type', 'application/zip, application/octet-stream, application/x-zip-compressed, multipart/x-zip')
-                        res.setHeader('Location', '/')
                         const
                             zipPath = file.path.split('/').slice(0, -1).join('/'),
                             readStream = fs.createReadStream(zipPath + '/' + zipName),
@@ -40,17 +38,19 @@ app.post('/submit-form', (req, res) => {
                             })
                     })
                 })
+            } else {
+                fs.unlink(file.path, err => {
+                    if (err) console.error(err)
+                })
+                res.redirect('/')
             }
-        })
-        .on('aborted', () => {
-            console.error('Request aborted by the user')
         })
         .on('error', (err) => {
             console.error('Error', err)
             throw err
         })
         .on('end', () => {
-            console.log('READY!')
+            console.log('Finished')
         })
 })
 
